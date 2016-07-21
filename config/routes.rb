ddf3c7de19a57a4d75acbc87fdb631e26c6f50f4
Sebastian Schkudlara <sebastian.schkudlara@gmail.com>
2016-07-21 11:36:45 +0200

    Allow to update dataset

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   scope module: :v1, constraints: APIVersion.new(version: 1, current: true) do
     post   'query/:id',    to: 'connectors#show'
     post   'datasets',     to: 'connectors#create'
+    post   'datasets/:id', to: 'connectors#update'
     delete 'datasets/:id', to: 'connectors#destroy'
     get    'info',         to: 'connectors#info'
   end
