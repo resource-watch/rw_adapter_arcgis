981de77d569401eaf48cf2457f5d359c12aaa62c
Sebastian Schkudlara <sebastian.schkudlara@gmail.com>
2016-08-10 09:31:17 +0200

    Allow up to 100 simultaneous connections

--- app/services/connector_service.rb
+++ app/services/connector_service.rb
@@ -35,20 +35,18 @@ class ConnectorService
 
       @request.on_complete do |response|
         if response.success?
-          # cool
+          @data = Oj.load(response.body.force_encoding(Encoding::UTF_8))[data_path] || Oj.load(response.body.force_encoding(Encoding::UTF_8))
         elsif response.timed_out?
-          'got a time out'
+          @data = 'got a time out'
         elsif response.code.zero?
-          response.return_message
+          @data = response.return_message
         else
-          'HTTP request failed: ' + response.code.to_s
+          @data = Oj.load(response.body)
         end
       end
-
       hydra.queue @request
       hydra.run
-
-      Oj.load(@request.response.body.force_encoding(Encoding::UTF_8))[data_path] || Oj.load(@request.response.body.force_encoding(Encoding::UTF_8))
+      @data
     end
   end
 end
