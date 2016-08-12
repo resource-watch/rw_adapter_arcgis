b2f2ae25b78a2149b429c0815293a241c2d6cdb5
Sebastian Schkudlara <sebastian.schkudlara@gmail.com>
2016-08-12 11:18:09 +0200

    version/flat-json

--- config/initializers/ams.rb
+++ config/initializers/ams.rb
@@ -1,3 +1,3 @@
-# AMS Adapter
-ActiveModelSerializers.config.adapter = ActiveModelSerializers::Adapter::JsonApi
-ActiveModelSerializers.config.key_transform = :unaltered
+# # AMS Adapter
+# ActiveModelSerializers.config.adapter = ActiveModelSerializers::Adapter::JsonApi
+# ActiveModelSerializers.config.key_transform = :unaltered
