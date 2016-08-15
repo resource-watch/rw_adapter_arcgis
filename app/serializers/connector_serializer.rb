1c277881aae30d2c286ac998e31827ed8f9913da
Sebastian Schkudlara <sebastian.schkudlara@gmail.com>
2016-08-15 19:47:53 +0200

    Add clone url to connector serializer

--- app/serializers/connector_serializer.rb
+++ app/serializers/connector_serializer.rb
@@ -1,12 +1,37 @@
 class ConnectorSerializer < ActiveModel::Serializer
-  attributes :data
+  attributes :clone_url, :data
+
+  def clone_url
+    data = {}
+    data['http_method'] = 'POST'
+    data['url']         = "#{URI.parse(clone_uri)}"
+    data['body']        = body_params
+    data
+  end
 
   def data
     object.data(@query_filter)
   end
 
+  def uri
+    "#{@uri['api_gateway_url']}#{@uri['full_path']}"
+  end
+
+  def clone_uri
+    "#{@uri['api_gateway_url']}/datasets/#{object.id}/clone"
+  end
+
+  def body_params
+    {
+      "dataset" => {
+        "dataset_url" => "#{URI.parse(uri)}"
+      }
+    }
+  end
+
   def initialize(object, options)
     super
     @query_filter = options[:query_filter]
+    @uri          = options[:uri]
   end
 end
